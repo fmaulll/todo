981cb1289f666d1b084000cef04dcced885e5918
fmaulll <maul2821@gmail.com>
2023-01-21 18:55:01 +0700

    initialize library gin gorm

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/fmaulll/todolist/controllers"
+	"github.com/fmaulll/todolist/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	models.ConnectDatabase()
+
+	router.GET("/api/todos", controllers.Index)
+	router.POST("/api/todos", controllers.Create)
+	router.PATCH("/api/todos/:id", controllers.Update)
+	router.DELETE("/api/todos", controllers.Delete)
+
+	router.Run()
+}
