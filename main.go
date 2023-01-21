72019ace1cfe2a6528888633667ae46eb7b1e9e5
fmaulll <maul2821@gmail.com>
2023-01-22 00:48:07 +0700

    fix cors with gin cors

--- main.go
+++ main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"os"
+	"time"
 
 	"github.com/fmaulll/todolist/controllers"
 	"github.com/fmaulll/todolist/models"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 	models.ConnectDatabase()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://foo.com"},
+		AllowMethods:     []string{"PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	router.GET("/api/todos", controllers.Index)
 	router.POST("/api/todos", controllers.Create)
