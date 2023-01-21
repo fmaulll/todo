2b9459fa2e8e579f5fa29270df336ce629e763af
fmaulll <maul2821@gmail.com>
2023-01-22 00:23:40 +0700

    removed column for migrate

--- models/todo.go
+++ models/todo.go
@@ -5,6 +5,5 @@ type Todo struct {
 	Title       string `gorm:"type:varchar(300)" json:"title"`
 	Description string `gorm:"type:varchar(300)" json:"description"`
 	CreatedAt   int    `gorm:"autoCreateTime:mili" json:"createdAt"`
-	ClosedAt    int    `gorm:"autoUpdateTime:mili" json:"closedAt"`
 	Completed   int    `gorm:"default:0" json:"completed"`
 }
