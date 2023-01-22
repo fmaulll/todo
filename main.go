d9af9ab8f4343e9c284bd328abaab95c8d7142d5
fmaulll <maul2821@gmail.com>
2023-01-22 12:41:41 +0700

    fix ?

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"time"
 
 	"github.com/fmaulll/todolist/controllers"
 	"github.com/fmaulll/todolist/models"
@@ -13,10 +14,17 @@ func main() {
 	router := gin.Default()
 	models.ConnectDatabase()
 
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"https://todo-production-dd3d.up.railway.app"}
-
-	router.Use(cors.New(config))
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://todo-production-dd3d.up.railway.app/"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	router.GET("/api/todos", controllers.Index)
 	router.POST("/api/todos", controllers.Create)
